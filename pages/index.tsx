a17854d4cec9ea3f13397cf60471417641179704
eleeyeah <ilietmks@gmail.com>
2023-06-19 16:08:44 +0200

    get dynamic data from Dynamo w/ the graphql api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { API } from "aws-amplify";
+import { quotesQueryName } from "@/src/graphql/queries";
+import { GraphQLResult } from "@aws-amplify/api-graphql";
 
 //Components
 import {
   BackgroundImage1,
-  BackgroundImage2,
   FooterContainer,
   CustomLink,
   GradientBackgroundCon,
@@ -20,10 +22,68 @@ import {
 
 //Assets
 import speechBubble from "@/assets/speechBubble.png";
-import quotes from "@/assets/quotes.png";
+
+// interface for DynamoDB object
+interface UpdateQuoteInfoData {
+  id: string;
+  queryName: string;
+  quotesGenerated: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// type quard for the fetch function
+function isGraphQLResultForquotesQueryName(
+  response: any
+): response is GraphQLResult<{
+  quotesQueryName: {
+    items: [UpdateQuoteInfoData];
+  };
+}> {
+  return (
+    response.data &&
+    response.data.quotesQueryName &&
+    response.data.quotesQueryName.items
+  );
+}
 
 export default function Home() {
-  const [quoteCounter, setQuoteCounter] = useState<Number | null>(0);
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
+  // quotes generated (DynamoDB object fetch function)
+  const updateQuoteInfo = async () => {
+    try {
+      const response = await API.graphql<UpdateQuoteInfoData>({
+        query: quotesQueryName,
+        authMode: "AWS_IAM",
+        variables: {
+          queryName: "LIVE",
+        },
+      });
+
+      console.log("response from DynamoDB", response);
+      // setNumberOfQuotes();
+
+      // create the type guards
+      if (!isGraphQLResultForquotesQueryName(response)) {
+        throw new Error("Response is not of type GraphQLResult");
+      }
+
+      if (!response.data) {
+        throw new Error("Response data is undefined");
+      }
+
+      const receivedNumberOfQuotes =
+        response.data.quotesQueryName.items[0].quotesGenerated;
+      setNumberOfQuotes(receivedNumberOfQuotes);
+    } catch (error) {
+      console.log("error getting data", error);
+    }
+  };
+
+  useEffect(() => {
+    updateQuoteInfo();
+  }, []);
 
   return (
     <>
@@ -38,7 +98,9 @@ export default function Home() {
       <GradientBackgroundCon>
         {/* Quote Generator Modal */}
         {/* <QuoteGeneratorModal/> */}
+
         {/* Quote Generator */}
+
         <QuoteGeneratorContainer>
           <QuoteGeneratorInnerContainer>
             <QuoteGeneratorTitle>
@@ -64,15 +126,13 @@ export default function Home() {
 
         {/* Background Images */}
         <BackgroundImage1 src={speechBubble} height={300} alt="speech bubble" />
-        {/* fix this image's css */}
-        {/* <BackgroundImage2 src={speechBubble} alt="speech bubble" /> */}
 
         {/* Footer */}
         <FooterContainer>
           <>
-            Quotes Generated:{quoteCounter}
+            Quotes Generated: {numberOfQuotes}
             <br />
-            Developed with <RedSpan>🎔</RedSpan> by{" "}
+            Developed with <RedSpan>🎔 </RedSpan> by{" "}
             <CustomLink
               href="https://portfolio-iliehonciuc.vercel.app/"
               target="_blank"
